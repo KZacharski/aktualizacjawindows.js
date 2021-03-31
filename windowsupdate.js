a435583a6b14ff6a8e9da87f6a5e9f511343d15c
Piotr Badełek <piotrbadelek@protonmail.com>
2021-03-31 18:13:37 +0200

    Add files via upload

---
+++ windowsupdate.js
@@ -0,0 +1,72 @@
+"use strict";
+
+window.windowsUpdate = 0.9;
+
+let interval = setInterval(() => {
+	if (Math.random() > window.windowsUpdate) {
+		// time for updates
+		let overlay = document.createElement("div");
+		overlay.setAttribute("class", "windowsupdate__overlay");
+		document.body.appendChild(overlay);
+		let h = document.createElement("div");
+		h.setAttribute("class", "windowsupdate");
+		h.innerHTML = `<h1>We're sorry, but this is important.</h1>
+			<p>The newest ${window.location.hostname} feature update is ready to install. We need you to kick it off.
+			With new features and apps, this one could take a little longer than other updates.<br><br>
+			Ready? Restart now. Not ready? We will restart for you in <span id="windowsupdate-counter">16</span> seconds.</p>
+			<button onclick="location.reload(true);">Restart now</button>`;
+		document.body.appendChild(h);
+		let cement = document.createElement("style");
+		cement.innerHTML = `.windowsupdate {
+			background-color: #0078d4;
+			color: white;
+			font-family: "Segoe UI", Ubuntu, -apple-system, BlinkMacSystemFont, Roboto, Oxygen, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+			padding: 20px;
+			position: fixed;
+			top: 50%;
+   			left: 50%;
+    		margin-right: -50%;
+    		transform: translate(-50%, -50%);
+			z-index: 9999;
+		}
+		
+		.windowsupdate button {
+			background-color: rgb(0, 120, 212);
+			border: 1px solid white;
+			padding: .5rem .9rem;
+			font-size: 1.05rem;
+			color: white;
+			cursor: pointer;
+			transition: .1s;
+		}
+		
+		.windowsupdate button:hover {
+			background-color: rgb(0, 106, 187);
+		}
+		
+		.windowsupdate__overlay {
+			position: fixed;
+			top: 0;
+			left: 0;
+			width: 100%;
+			height: 100%;
+			z-index: 999;
+			background-color: black;
+			opacity: .8;
+		}`;
+		document.body.appendChild(cement);
+		clearInterval(interval);
+		let time = 16;
+		setInterval(() => {
+			time--;
+			document.querySelector("#windowsupdate-counter").innerText = time;
+			if (time === 0) {
+				document.querySelector(".windowsupdate h1").innerText = "Restarting...";
+				document.querySelector(".windowsupdate p").innerText = "Please wait...";
+				setTimeout(() => {
+					window.location.reload();
+				}, 1024);
+			};
+		}, 1000);
+	};
+}, 1000);
